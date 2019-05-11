9222fd940d65d84351a61c58c4040b1fc7a488b2
rstutalo <richardtutalo@gmail.com>
2019-05-10 19:49:53 -0600

    server.js file set up with dependencies and db connection

---
+++ server.js
@@ -0,0 +1,31 @@
+// DEPENDENCIES
+
+//require express
+const express = require("express");
+//require mongoose
+const mongoose = require("mongoose");
+//require all of the routes
+const routes = require("./routes");
+
+//instantiate express
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//middleware setup
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//serve static routes 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+//SET UP CONNECTION  TO MONGO
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch");
+
+//START SERVER FOR API SEARCHING
+app.listen(PORT, function() {
+    console.log(`The Server is listening on PORT ${PORT}`);
+});
