ec8a5bc3c858dd6f43ffbeeddd147ac0b0d79b17
rstutalo <richardtutalo@gmail.com>
2019-05-11 14:26:14 -0600

    updated App.js to reflect proper routing for all other components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import react and all other dependencies and components
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SaveBooks from "./pages/SaveBooks";
+import SearchBooks from "./pages/SearchBooks";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav>
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SaveBooks} />
+            <Route exact path="/saved/:id" component={SaveBooks} />
+            <Route compnent={NoMatch} />
+          </Switch>
+        </Nav>
+        {/* Self closing footer tag */}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
+//export application
 export default App;
