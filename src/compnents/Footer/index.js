d31a599bb95f966c417281312e36f066989f7363
rstutalo <richardtutalo@gmail.com>
2019-05-11 19:49:01 -0600

    footer component coded and styled.

---
+++ src/compnents/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+    <footer className="page-footer">
+        <div className = "container-fluid text-center">
+            <p>Powered by Google</p>
+        </div>
+    </footer>
+);
